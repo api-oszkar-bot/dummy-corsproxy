0b91d4eea64ef22065f02769b66d4f2dbde52274
Fedor Korshunov <mail@fedor.cc>
2015-06-28 16:02:09 +0800

    forward original response status code and headers

--- index.js
+++ index.js
@@ -2,18 +2,23 @@
 var http = require('http')
 var request = require('request')
 
-var port = process.env.PORT || 8080
+var port = process.env.PROXY_PORT || 8080
 
 http.createServer(function (req, res) {
   res.setTimeout(25000)
-  res.setHeader('Access-Control-Allow-Origin', '*')
   try {
-    request(req.url.slice(1), {encoding: null}, function(error, response, body) {
+    var url = req.url.slice(1)
+    console.log('GET '+url);
+    request(url, {encoding: null}, function(error, response, body) {
+      response.headers['Access-Control-Allow-Origin'] = '*'
+      res.writeHead(response.statusCode, response.headers)
       res.write(body)
       res.end()
     })
   }
-  catch(e) {}
+  catch(e) {
+    console.log('GET '+url+' failed\n\t'+e)
+  }
 }).listen(port)
 
 console.log("Listening on port: " + port)
